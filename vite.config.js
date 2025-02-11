b1bd8740fae62910d08bb1884887d977017f314d
ganeshmetku <144722060+Ganumetku@users.noreply.github.com>
2025-02-11 13:14:28 +0530

    hello heurfjj

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import vercel from 'vite-plugin-vercel';
 
 export default defineConfig({
-  plugins: [react(),vercel()],
-  base: "/Portfolio",
+  plugins: [react()],
+  base: "/",
 });
