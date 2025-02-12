ab7f6519da55d0f90deec6490310298821a0dd37
ganeshmetku <144722060+Ganumetku@users.noreply.github.com>
2025-02-12 12:24:53 +0530

    Deploying to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Portfolio/', // The base path for your GitHub Pages project
   plugins: [react()],
-  base: '/Portfolio/',  // Use the exact repository name
 });
