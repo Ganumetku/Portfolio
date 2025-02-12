76296f1d0ae2ac845f01801a9bd12c505f0e0feb
ganeshmetku <144722060+Ganumetku@users.noreply.github.com>
2025-02-12 11:55:17 +0530

    Save progress before deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import vercel from 'vite-plugin-vercel';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: '/Portfolio/',  // Use the exact repository name
 });
