348057b024b5740edd653e8cfca0f8ad51f0e94a
ganeshmetku <144722060+Ganumetku@users.noreply.github.com>
2025-02-12 13:51:24 +0530

    fidt

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Portfolio/', // The base path for your GitHub Pages project
+  base: '/Portfolio/react-portfolio/', // The base path for your GitHub Pages project
   plugins: [react()],
 });
